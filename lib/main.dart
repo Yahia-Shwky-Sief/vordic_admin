d0e552a9f87bfbf9ef29264aa085952bddf681af
YAHIA-SHWKY-SIEF <yahiasief4@gmail.com>
2025-03-30 17:12:46 +0200

    Add Firebase and Supabase integration with necessary configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vordic_admin/core/theme/theme.dart';
 import 'package:vordic_admin/features/nav/nav.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:vordic_admin/firebase_options.dart';
+import 'package:vordic_admin/core/constants/supabase_api_keys.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: SupabaseApiKeys.url,
+    anonKey: SupabaseApiKeys.anonKey,
+  );
   runApp(const MyApp());
 }
 
