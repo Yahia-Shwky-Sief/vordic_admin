60f538b43604224ec99603df8530ad09ffe822ee
YAHIA-SHWKY-SIEF <yahiasief4@gmail.com>
2025-03-31 03:49:30 +0200

    Refactor ad banner storage methods to upload files to a specific directory and retrieve public URLs

--- lib/features/ad_banner_management/logic/storage.dart
+++ lib/features/ad_banner_management/logic/storage.dart
@@ -5,24 +5,20 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class Storage {
   final SupabaseStorageClient storage = Supabase.instance.client.storage;
 
-  Future<void> uploadFile({
+  uploadAdBannerAndGetPublicUrl({
     required String filePath,
     required String fileName,
   }) async {
     File file = File(filePath);
 
     try {
-      await storage.from('images').upload(filePath, file);
+      await storage.from('images').upload("ads_banners/$fileName", file);
     } catch (e) {
       print('Error uploading file: $e');
     }
-  }
-
-  Future<String> downloadUrl({
-    required String imageName,
-  }) async {
-    String downloadUrl =
-        await storage.from('images').createSignedUrl('images/$imageName', 60);
+    String downloadUrl = storage.from('images').getPublicUrl(
+          'ads_banners/$fileName',
+        );
     return downloadUrl;
   }
 }
--- lib/features/ad_banner_management/ui/ad_banner_management.dart
+++ lib/features/ad_banner_management/ui/ad_banner_management.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:vordic_admin/core/widgets/nav/nav.dart';
 import 'package:vordic_admin/core/widgets/text_form_field.dart';
@@ -31,178 +33,167 @@ class _AdBannerManagementState extends State<AdBannerManagement> {
               icon: const Icon(Icons.add),
               onPressed: () {
                 // Add your action here, e.g., show a dialog to add a new ad banner
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Add New Ad Banner'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Form(
-                            key: formKey,
-                            child: Column(
-                              children: [
-                                textFormField(
-                                  hintText: 'Enter Ad Banner Name',
-                                  controller: controller,
-                                  keyboardType: TextInputType.text,
-                                  obscureText: false,
-                                  validator: (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter a value';
-                                    }
-                                    return null;
-                                  },
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    MaterialButton(
-                                      onPressed: () async {
-                                        final result =
-                                            await ImagePicker().pickImage(
-                                          source: ImageSource.gallery,
-                                        );
-                                        if (result == null) {
-                                          return;
-                                        }
-                                        final path = result.path;
-                                        fileName = result.name;
+                //   showDialog(
+                //     context: context,
+                //     builder: (context) {
+                //       return AlertDialog(
+                //         title: const Text('Add New Ad Banner'),
+                //         content: Column(
+                //           mainAxisSize: MainAxisSize.min,
+                //           children: [
+                //             Form(
+                //               key: formKey,
+                //               child: Column(
+                //                 children: [
+                //                   textFormField(
+                //                     hintText: 'Enter Ad Banner Name',
+                //                     controller: controller,
+                //                     keyboardType: TextInputType.text,
+                //                     obscureText: false,
+                //                     validator: (value) {
+                //                       if (value == null || value.isEmpty) {
+                //                         return 'Please enter a value';
+                //                       }
+                //                       return null;
+                //                     },
+                //                   ),
+                //                   const SizedBox(height: 10),
+                //                   Row(
+                //                     children: [
+                //                       MaterialButton(
+                //                         onPressed: () async {
+                //                           final result =
+                //                               await ImagePicker().pickImage(
+                //                             source: ImageSource.gallery,
+                //                           );
+                //                           if (result == null) {
+                //                             return;
+                //                           }
+                //                           final path = result.path;
+                //                           fileName = result.name;
 
-                                        await storage.uploadFile(
-                                          filePath: path,
-                                          fileName: fileName,
-                                        );
+                //                           await storage.uploadFile(
+                //                             filePath: path,
+                //                             fileName: fileName,
+                //                           );
 
-                                        image = await storage.downloadUrl(
-                                          imageName: fileName,
-                                        );
-                                        setState(() {});
-                                      },
-                                      child: Container(
-                                        child: Center(
-                                          child: Text('Select Image',
-                                              style: TextStyle(
-                                                  color: Colors.white)),
-                                        ),
-                                      ),
-                                    ),
-                                    const Spacer(),
-                                    SizedBox(
-                                        width: 100,
-                                        height: 100,
-                                        child: image == ''
-                                            ? Container(
-                                                color: Colors.black87,
-                                              )
-                                            : Image(
-                                                image: NetworkImage(image))),
-                                    const Spacer(),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      actions: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            TextButton(
-                              onPressed: () {
-                                // Add your action to add a new ad banner here
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('Add'),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(),
-                              child: const Text('Close'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    );
-                  },
-                );
+                //                           image = await storage.downloadUrl(
+                //                             imageName: fileName,
+                //                           );
+                //                           setState(() {});
+                //                         },
+                //                         child: Container(
+                //                           child: Center(
+                //                             child: Text('Select Image',
+                //                                 style: TextStyle(
+                //                                     color: Colors.white)),
+                //                           ),
+                //                         ),
+                //                       ),
+                //                       const Spacer(),
+                //                       SizedBox(
+                //                           width: 100,
+                //                           height: 100,
+                //                           child: image == ''
+                //                               ? Container(
+                //                                   color: Colors.black87,
+                //                                 )
+                //                               : Image(
+                //                                   image: NetworkImage(image))),
+                //                       const Spacer(),
+                //                     ],
+                //                   ),
+                //                 ],
+                //               ),
+                //             ),
+                //           ],
+                //         ),
+                //         actions: [
+                //           Row(
+                //             mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //             children: [
+                //               TextButton(
+                //                 onPressed: () {
+                //                   // Add your action to add a new ad banner here
+                //                   Navigator.of(context).pop();
+                //                 },
+                //                 child: const Text('Add'),
+                //               ),
+                //               TextButton(
+                //                 onPressed: () => Navigator.of(context).pop(),
+                //                 child: const Text('Close'),
+                //               ),
+                //             ],
+                //           ),
+                //         ],
+                //       );
+                //     },
+                //   );
               },
             ),
           ],
         ),
-        body:  Center(
+        body: Center(
           child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Form(
-                            key: formKey,
-                            child: Column(
-                              children: [
-                                textFormField(
-                                  hintText: 'Enter Ad Banner Name',
-                                  controller: controller,
-                                  keyboardType: TextInputType.text,
-                                  obscureText: false,
-                                  validator: (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter a value';
-                                    }
-                                    return null;
-                                  },
-                                ),
-                                const SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    MaterialButton(
-                                      onPressed: () async {
-                                        final result =
-                                            await ImagePicker().pickImage(
-                                          source: ImageSource.gallery,
-                                        );
-                                        if (result == null) {
-                                          return;
-                                        }
-                                        final path = result.path;
-                                        fileName = result.name;
-
-                                        await storage.uploadFile(
-                                          filePath: path,
-                                          fileName: fileName,
-                                        );
-
-                                        image = await storage.downloadUrl(
-                                          imageName: fileName,
-                                        );
-                                        setState(() {});
-                                      },
-                                      child: Container(
-                                        child: Center(
-                                          child: Text('Select Image',
-                                              style: TextStyle(
-                                                  color: Colors.white)),
-                                        ),
-                                      ),
-                                    ),
-                                    const Spacer(),
-                                    SizedBox(
-                                        width: 100,
-                                        height: 100,
-                                        child: image == ''
-                                            ? Container(
-                                                color: Colors.black87,
-                                              )
-                                            : Image(
-                                                image: NetworkImage(image))),
-                                    const Spacer(),
-                                  ],
-                                ),
-                              ],
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    textFormField(
+                      hintText: 'Enter Ad Banner Name',
+                      controller: controller,
+                      keyboardType: TextInputType.text,
+                      obscureText: false,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a value';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        MaterialButton(
+                          onPressed: () async {
+                            final result = await ImagePicker()
+                                .pickImage(source: ImageSource.gallery);
+                            if (result == null) {
+                              print('No image selected');
+                              return;
+                            }
+                            image = await storage.uploadAdBannerAndGetPublicUrl(
+                              filePath: result.path,
+                              fileName: result.name,
+                            );
+                            setState(() {});
+                          },
+                          child: Container(
+                            child: Center(
+                              child: Text('Select Image',
+                                  style: TextStyle(color: Colors.white)),
                             ),
                           ),
-                        ],
-                      ),
-                      
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: image == ''
+                                ? Container(
+                                    color: Colors.black87,
+                                  )
+                                : Image.network(image)),
+                        const Spacer(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
